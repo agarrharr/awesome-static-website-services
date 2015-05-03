2e872c719be3e22f15e50d45f52bb51782858cfe
adam-harris <adam.harris@domo.com>
2015-05-03 07:40:17 -0600

    Add cloudcannon

---
+++ gitRewrite.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+ 
+git filter-branch --env-filter '
+
+OLD_EMAIL="adam.harris@domo.com"
+CORRECT_NAME="aharris88"
+CORRECT_EMAIL="aharris88@gmail.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
